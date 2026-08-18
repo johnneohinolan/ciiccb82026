2ae6db361aabc752acf6f20a5ebb0652c96ac104
johnneohinolan <johnneohinolan18@gmail.com>
2026-08-18 08:24:38 +0000

    override

---
+++ Task14/abstractclass.java
@@ -0,0 +1,31 @@
+abstract class abstractClass {
+    abstract void abstractMethod();
+
+    void concreteMethod() {
+        System.out.println("This is a concrete method.");
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.abstractMethod();
+        b.concreteMethod();
+
+        C c = new C();
+        c.abstractMethod();
+        c.concreteMethod();
+    }
+}
+
+class B extends abstractClass {
+    @Override
+    void abstractMethod() {
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+class C extends abstractClass {
+    @Override
+    void abstractMethod() {
+        System.out.println("C's implementation of abstractMethod.");
+    }
+}
